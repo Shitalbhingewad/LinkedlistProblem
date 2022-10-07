118ed2171717f7090c1348df73fdb9501ce98ec1
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-07 16:58:13 +0530

    Logic added for delete any node using index

--- src/main/java/com/LinkedListProblem/App.java
+++ src/main/java/com/LinkedListProblem/App.java
@@ -8,13 +8,15 @@ public class App {
     public static void main( String[] args ){
         logger.info( "Welcome to LinkedList Problems" );
         Linkedlist list = new Linkedlist();
-     // list.insertNode(56);
+        // list.insertNode(56);
         // list.insertNode(30);
         // list.insertNode(70);
         list.insertAtStart(70);
         // list.insertNode(30);
         list.insertAtStart(56);
         list.insertAtIndex(1, 30);
+        // list.deleteNode(0);
+        list.deleteNode(0);
         list.showLinkedList();
        
     }
--- src/main/java/com/LinkedListProblem/Linkedlist.java
+++ src/main/java/com/LinkedListProblem/Linkedlist.java
@@ -3,7 +3,7 @@ package com.LinkedListProblem;
 public class Linkedlist {
     Node head;
 
-  void insertAtStart(int data) {
+    void insertAtStart(int data) {
         Node node = new Node();
         node.data = data;
         node.next = null;
@@ -32,7 +32,8 @@ public class Linkedlist {
             temp.next = node;
         }
     }
-  void insertAtIndex(int index, int data) {
+
+    void insertAtIndex(int index, int data) {
         Node node = new Node();
         node.data = data;
         node.next = null;
@@ -48,6 +49,7 @@ public class Linkedlist {
             temp.next = node;
         }
     }
+
     void showLinkedList() {
         Node temp = head;
         while (temp.next != null) {
@@ -55,7 +57,31 @@ public class Linkedlist {
             temp = temp.next;
         }
         System.out.println(temp.data);
-
     }
 
+    void deleteNode(int index) {
+        Node node;
+        Node temp;
+        temp = head;
+        if (head == null) {
+            System.out.println("NO list exists!!");
+        } else {
+
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            if (index == 0) {
+
+                head = head.next;
+                temp = null;
+
+            } else {
+                node = temp.next;
+                temp.next = node.next;
+                node = null;
+            }
+
+        }
+
+    }
 }
